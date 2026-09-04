3c99293ae37542ffe8d3cd3ae56313ecc7a3837a
BOOBESH K <boobesh24bcs049@gmail.com>
2026-09-04 22:51:02 +0530

    Time: 0 ms (100.00%) | Memory: 30.1 MB (99.59%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=nums.size();
-        if(n==0) return (nums[0]-nums[0])<=k ? nums[0]-nums[0]: -1; 
+        if(n==0) return -1; 
         for(int j=0;j<n;j++)
         {
             int max=nums[0];
