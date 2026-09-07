049e453193977f4257b5dfb583f998793d2b4a5e
BOOBESH K <boobesh24bcs049@gmail.com>
2026-09-07 19:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (38.87%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        const int MOD = 1e9 + 7;
+        int n = s.size();
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+        vector<int> last(26, -1);
+
+        for (int i = 1; i <= n; i++) {
+            int idx = s[i - 1] - 'a';
+            dp[i] = (2 * dp[i - 1]) % MOD;
+            if (last[idx] != -1) {
+                dp[i] = (dp[i] - dp[last[idx]] + MOD) % MOD;
+            }
+            last[idx] = i - 1;
+        }
+
+        return (int)((dp[n] - 1 + MOD) % MOD);
+    }
+};
