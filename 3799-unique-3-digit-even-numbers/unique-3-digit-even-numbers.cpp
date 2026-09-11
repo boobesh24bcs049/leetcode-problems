da747f5145fee2417641ada337d416522448ecfb
BOOBESH K <boobesh24bcs049@gmail.com>
2026-09-12 00:02:56 +0530

    Time: 4 ms (79.08%) | Memory: 28.8 MB (88.78%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        set<int> s;
+        int cnt[10] = {0};
+        for (int d : digits) cnt[d]++;
 
-        int n = digits.size();
+        int total = 0;
+        for (int h = 1; h <= 9; h++) {          
+            for (int t = 0; t <= 9; t++) {
+                for (int u = 0; u <= 8; u += 2) { 
+                    int need[10] = {0};
+                    need[h]++; need[t]++; need[u]++;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
+                    bool ok = true;
+                    for (int d = 0; d <= 9 && ok; d++)
+                        if (need[d] > cnt[d]) ok = false;
 
-
-                    if (i == j || j == k || i == k)
-                        continue;
-
-                    if (digits[i] == 0)
-                        continue;
-
-                    if (digits[k] % 2 != 0)
-                        continue;
-
-                    int num = digits[i] * 100 +
-                              digits[j] * 10 +
-                              digits[k];
-
-                    s.insert(num);
+                    if (ok) total++;
                 }
             }
         }
-
-        return s.size();
+        return total;
     }
 };
