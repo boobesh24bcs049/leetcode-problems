d40ec5322af9cb4dc942ef03b1d2ea3ff936e78a
BOOBESH K <boobesh24bcs049@gmail.com>
2026-09-11 23:31:06 +0530

    Time: 3 ms (84.06%) | Memory: 30.9 MB (66.84%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -9,15 +9,13 @@ public:
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < n; k++) {
 
-                    // Cannot use the same array element twice
+
                     if (i == j || j == k || i == k)
                         continue;
 
-                    // First digit cannot be 0
                     if (digits[i] == 0)
                         continue;
 
-                    // Last digit must be even
                     if (digits[k] % 2 != 0)
                         continue;
 
