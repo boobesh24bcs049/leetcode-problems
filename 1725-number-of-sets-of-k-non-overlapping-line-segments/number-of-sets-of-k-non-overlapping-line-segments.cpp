3e26712f70cfea8e72a2281e55c338e59e561b30
BOOBESH K <boobesh24bcs049@gmail.com>
2026-09-16 23:38:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (94.86%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    const int MOD=1e9+7;
+    long long pow(int x, int n){
+        long long base=x, ans=1;
+        while(n){
+         if(n&1){
+            ans = (ans *base)%MOD;
+         }
+         base = (base*base)%MOD;
+         n>>=1;
+        }
+        return ans;
+    }
+    long long inv(int n){
+        return pow(n,MOD-2);
+    }
+    int C(int n , int r ){
+     long long nr=1,dr1=1,dr2=1;
+     for(int i=1;i<=n;i++)nr= (nr*i)%MOD;
+     for(int i=n-r;i>=1;i--)dr1= (dr1*i)%MOD;
+     for(int i=r;i>=1;i--)dr2=(dr2*i)%MOD;
+     return nr * inv(dr1)%MOD * inv(dr2) %MOD; 
+    }
     int numberOfSets(int n, int k) {
-        const int m = 1e9 + 7;
-
-        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
-
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
-
-        for (int segments = 1; segments <= k; segments++) {
-            long long prefixSum = 0;
-
-            for (int points = 1; points <= n; points++) {
-                if (points >= 2) {
-                    prefixSum = (prefixSum + dp[points - 1][segments - 1]) % m;
-                }
-
-                dp[points][segments] =
-                    (dp[points - 1][segments] + prefixSum) % m;
-            }
-        }
-
-        return dp[n][k];
+        return C(n+k-1,2*k);
     }
 };
