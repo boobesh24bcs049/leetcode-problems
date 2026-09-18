9b5cc9d3d96579e906172e5e53f8f43352a2423d
BOOBESH K <boobesh24bcs049@gmail.com>
2026-09-18 22:55:01 +0530

    Time: 2 ms (79.74%) | Memory: 13.3 MB (72.32%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-    vector<string> s;
+    vector<string> ans;
 
     vector<string> generateParenthesis(int n) {
-        vector<string> s1;
-
-        generate("", n);
-
-        for (int j = 0; j < s.size(); j++) {
-            int balance = 0;
-            string a = s[j];
-            bool valid = true;
-
-            for (int i = 0; i < a.length(); i++) {
-                if (a[i] == '(') {
-                    balance++;
-                }
-                else {
-                    balance--;
-
-                    if (balance < 0) {
-                        valid = false;
-                        break;
-                    }
-                }
-            }
-
-            if (valid && balance == 0) {
-                s1.push_back(a);
-            }
-        }
-
-        return s1;
+        string s;
+        s.reserve(2 * n);
+        solve(s, 0, 0, n);
+        return ans;
     }
 
-    void generate(string m, int n) {
-        if (m.length() == 2 * n) {
-            s.push_back(m);
+    void solve(string& s, int open, int close, int n) {
+        if (s.size() == 2 * n) {
+            ans.push_back(s);
             return;
         }
 
-        generate(m + '(', n);
-        generate(m + ')', n);
+        if (open < n) {
+            s.push_back('(');
+            solve(s, open + 1, close, n);
+            s.pop_back();
+        }
+
+        if (close < open) {
+            s.push_back(')');
+            solve(s, open, close + 1, n);
+            s.pop_back();
+        }
     }
 };
